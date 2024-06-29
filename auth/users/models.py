59ff5d9fb67d892facac079e247e4a35220c4b3e
khaldfareed <khaldfareed@gmail.com>
2024-06-29 14:10:27 +0300

    Update views.py
    reset-code

--- auth/users/models.py
+++ auth/users/models.py
@@ -50,6 +50,7 @@ class Reservation(models.Model):
     exited_at = models.DateTimeField(null=True, blank=True)
     expires_at = models.DateTimeField(null=True, blank=True)  # Added expires_at field
     duration = models.FloatField(null=True, blank=True)  # New duration field
+
     def __str__(self):
         return f"Reservation {self.reservation_code} by {self.user.email}"
 
