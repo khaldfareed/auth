d6329f456bb2a05b534500671e117fbf2bd75f57
khaldfareed <khaldfareed@gmail.com>
2024-07-04 21:34:16 +0300

    Update views.py
    fina

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8-^pytn(yaw9f0k-v_o+ttzc(zgq&4mq%lilh8+we-d7p!hws-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
